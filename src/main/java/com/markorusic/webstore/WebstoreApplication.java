1a3609b2b716a3bbae4c0342710b687fdf43557e
Marko Rusic <markousp5@gmail.com>
2020-10-07 17:31:16 +0200

    init

---
+++ src/main/java/com/markorusic/webstore/WebstoreApplication.java
@@ -0,0 +1,13 @@
+package com.markorusic.webstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebstoreApplication.class, args);
+	}
+
+}

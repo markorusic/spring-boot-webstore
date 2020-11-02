384f5330caa855673d6401315870d75afcfa8e88
Marko Rusic <markousp5@gmail.com>
2020-11-02 22:14:54 +0100

    fix category q classes

--- src/main/java/com/markorusic/webstore/dao/CategoryDao.java
+++ src/main/java/com/markorusic/webstore/dao/CategoryDao.java
@@ -2,7 +2,7 @@ package com.markorusic.webstore.dao;
 
 import com.markorusic.webstore.domain.Category;
 import com.markorusic.webstore.domain.Product;
-import com.markorusic.webstore.domain.QProduct;
+import com.markorusic.webstore.domain.QCategory;
 import com.querydsl.core.types.dsl.StringExpression;
 import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,10 +11,10 @@ import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.querydsl.binding.SingleValueBinding;
 
-public interface CategoryDao extends JpaRepository<Category, Long>, QuerydslPredicateExecutor<Product>, QuerydslBinderCustomizer<QProduct> {
+public interface CategoryDao extends JpaRepository<Category, Long>, QuerydslPredicateExecutor<Category>, QuerydslBinderCustomizer<QCategory> {
 
     @Override
-    default void customize(QuerydslBindings bindings, QProduct root) {
+    default void customize(QuerydslBindings bindings, QCategory root) {
         bindings.bind(String.class)
                 .first((SingleValueBinding<StringPath, String>) StringExpression::containsIgnoreCase);
     }
--- src/main/java/com/markorusic/webstore/domain/Category.java
+++ src/main/java/com/markorusic/webstore/domain/Category.java
@@ -20,6 +20,6 @@ public class Category {
 
     private String name;
 
-    @OneToMany
+    @OneToMany(mappedBy = "category")
     private List<Product> products;
 }

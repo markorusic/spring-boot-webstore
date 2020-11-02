de2d185456234c7ba599316d71326995f66dacb2
igor.icelic <igoricelic@outlook.com>
2020-11-02 22:22:44 +0100

    -- querydsl fix

--- src/main/java/com/markorusic/webstore/domain/Product.java
+++ src/main/java/com/markorusic/webstore/domain/Product.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class Product {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;

9cf46a2fc0e54c704bd75b30b0125e618af76bb1
Marko Rusic <markousp5@gmail.com>
2020-12-02 18:28:52 +0100

    move protected route paths to Map<AuthRole, String[]>

--- src/main/java/com/markorusic/webstore/config/SecurityConfig.java
+++ src/main/java/com/markorusic/webstore/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.markorusic.webstore.config;
 import com.markorusic.webstore.security.AuthFilter;
 import com.markorusic.webstore.security.domain.AuthRole;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,7 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import java.util.Arrays;
+import java.util.Map;
 
 @Order(1)
 @Configuration
@@ -24,9 +23,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final AuthFilter authFilter;
 
+    private final Map<AuthRole, String[]> PROTECTED_ROUTES_MAP = Map.ofEntries(
+            Map.entry(AuthRole.Admin, new String[] {
+                    "/customers/update",
+                    "/customers/me",
+                    "/customers/me/actions",
+                    "/orders/me",
+                    "/orders/save",
+                    "/orders/cancel",
+                    "/product-reviews/save",
+                    "/product-reviews/update",
+                    "/product-reviews/delete"
+            }),
+            Map.entry(AuthRole.Customer, new String[] {
+                    "/admins/me",
+                    "/admins/me/actions",
+                    "/products/save",
+                    "/products/update",
+                    "/products/delete",
+                    "/categories/save",
+                    "/categories/update",
+                    "/categories/delete",
+                    "/orders/findAll",
+                    "/orders/findById",
+                    "/orders/ship",
+                    "/file-upload"
+            })
+    );
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.addFilterBefore( authFilter, UsernamePasswordAuthenticationFilter.class);
         http
             .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
             .httpBasic().disable()
@@ -34,32 +60,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .authorizeRequests()
-            .antMatchers("customers/update",
-                    "/customers/me",
-                    "/customers/me/actions",
-                    "/categories/save",
-                    "/orders/me",
-                    "/orders/save",
-                    "/orders/cancel",
-                    "/product-reviews/save",
-                    "/product-reviews/update",
-                    "/product-reviews/delete",
-                    "/categories/save"
-            ).hasAuthority(AuthRole.Customer.toString())
-            .antMatchers(
-                    "/admins/me",
-                    "/admins/me/actions",
-                    "/products/save",
-                    "/products/update",
-                    "/products/delete",
-
-                    "/categories/update",
-                    "/categories/delete",
-                    "/orders/findAll",
-                    "/orders/findById",
-                    "/orders/ship",
-                    "/file-upload"
-            ).hasAuthority(AuthRole.Admin.toString())
+                .antMatchers(PROTECTED_ROUTES_MAP.get(AuthRole.Customer)).hasAuthority(AuthRole.Customer.toString())
+                .antMatchers(PROTECTED_ROUTES_MAP.get(AuthRole.Admin)).hasAuthority(AuthRole.Admin.toString())
             .antMatchers("/**").permitAll();
 
 

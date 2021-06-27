c99a566b97691ef19556233cfd211c9f1d9f42c5
Marko Rusic <markousp5@gmail.com>
2021-06-27 14:56:28 +0200

    fix ProductReviewDao

--- src/main/java/com/markorusic/webstore/dao/ProductReviewDao.java
+++ src/main/java/com/markorusic/webstore/dao/ProductReviewDao.java
@@ -1,12 +1,17 @@
 package com.markorusic.webstore.dao;
 
 import com.markorusic.webstore.domain.ProductReview;
+import com.markorusic.webstore.domain.QProduct;
 import com.markorusic.webstore.domain.QProductReview;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.SingleValueBinding;
 
 import java.util.List;
 
@@ -16,4 +21,10 @@ public interface ProductReviewDao extends JpaRepository<ProductReview, Long>, Qu
     List<ProductReview> findByCustomerId(Long productId);
 
     List<ProductReview> findByProductIdAndCustomerId(Long productId, Long customerId);
+
+    @Override
+    default void customize(QuerydslBindings bindings, QProductReview root) {
+        bindings.bind(String.class)
+                .first((SingleValueBinding<StringPath, String>) StringExpression::containsIgnoreCase);
+    }
 }
